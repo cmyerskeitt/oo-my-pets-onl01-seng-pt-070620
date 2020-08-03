7539d98b7f325288dd6f8779bc5fd68000620107
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-08-03 20:56:44 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,64 @@
 class Owner
-  # code goes here
+  attr_accessor :pets
+  attr_reader :name, :species
+ 
+  
+  @@all = []
+  def initialize(name)
+    @name= name 
+    @species= "human"
+    @pets = {dogs => [], cats => []}
+    @@all<< self
+    
+  end 
+  
+  def say_species 
+    "I am a #{@species}."
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.count 
+    @@all.count
+  end 
+  
+  def self.reset_all
+    @@all.clear
+  end 
+  
+  def cats 
+    Cat.all.select {|cat| cat.owner == self} 
+  end 
+  
+  def dogs 
+    Dog.all.select {|dog| dog.owner == self} 
+  end 
+  
+  def buy_cat(name)
+    Cat.new(name,self)
+  end 
+
+  def buy_dog(name)
+    Dog.new(name,self)
+  end 
+  
+  def walk_dogs 
+    dogs.each {|dog| dog.mood = "happy"}
+  end
+  
+  def feed_cats 
+    cats.each {|cat| cat.mood = "happy"}
+  end 
+  
+  def sell_pets 
+     pets.each do |species, animal|
+      animal.each do |animal|
+        animal.mood = "nervous"
+        end
+      animal.clear
+    end
+  end
+  
 end
